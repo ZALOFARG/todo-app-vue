28f101a3930842c437fbe9c641b307b9d4b1a30d
ZALOFARG <andre.work.go@gmail.com>
2025-04-22 10:24:53 -0500

    feat: provide toggling button for rendering

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,27 @@
 import { ref } from 'vue'
 
 const header = ref('Shopping List App')
+const editing = ref(false)
 const items = ref([
-  { id: 1, label: '10 party hats' },
-  { id: 2, label: '2 board games' },
-  { id: 3, label: '20 cups' },
+  // { id: 1, label: '10 party hats' },
+  // { id: 2, label: '2 board games' },
+  // { id: 3, label: '20 cups' },
 ])
 const newItem = ref('')
 const newItemHighPriority = ref(false)
+const saveItem = () => {
+  items.value.push({ id: items.value.length + 1, label: newItem.value })
+  newItem.value = ''
+}
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
-  <form
-    class="add-item-form"
-    @submit.prevent="items.push({ id: items.length + 1, label: newItem })"
-  >
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button v-if="editing" class="btn">Cancel</button>
+    <button v-else class="btn btn-primary">Add Item</button>
+  </div>
+  <form class="add-item-form" @submit.prevent="saveItem" v-if="editing">
     <input v-model.trim="newItem" type="text" placeholder="Add an item..." />
     <label>
       <input type="checkbox" v-model="newItemHighPriority" />
@@ -27,4 +33,5 @@ const newItemHighPriority = ref(false)
   <ul>
     <li v-for="item in items" :key="item.id">{{ item.label }}</li>
   </ul>
+  <p v-if="!items.length">Nothing to see here</p>
 </template>
