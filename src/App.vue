ffd34892ecc95db7acf357df5d8449a385bc654d
ZALOFARG <andre.work.go@gmail.com>
2025-04-21 23:00:51 -0500

    feat: initial config and hello world basic

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const header = ref('Shopping List App')
+const items = ref([
+  { id: 1, label: '10 party hats' },
+  { id: 2, label: '2 board games' },
+  { id: 3, label: '20 cups' },
+])
+</script>
+
+<template>
+  <h1>{{ header }}</h1>
+  <ul>
+    <li v-for="item in items" :key="item.id">{{ item.label }}></li>
+  </ul>
+</template>
