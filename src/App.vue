94d6ecdb35b24f8df137e2ca6e502fc8231353c7
ZALOFARG <andre.work.go@gmail.com>
2025-04-21 23:39:29 -0500

    feat: aggregate a submit logic

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,23 @@ const items = ref([
   { id: 3, label: '20 cups' },
 ])
 const newItem = ref('')
-const newItemPriority = ref('low')
+const newItemHighPriority = ref(false)
 </script>
 
 <template>
   <h1>{{ header }}</h1>
-  <input v-model.trim="newItem" type="text" placeholder="Add an item..." />
-  Priority:
-  <label>
-    <input type="radio" v-model="newItemPriority" value="low" />
-    Low
-  </label>
-  <label>
-    <input type="radio" v-model="newItemPriority" value="high" />
-    High
-  </label>
+  <form
+    class="add-item-form"
+    @submit.prevent="items.push({ id: items.length + 1, label: newItem })"
+  >
+    <input v-model.trim="newItem" type="text" placeholder="Add an item..." />
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority" />
+      Priority
+    </label>
+    <button class="btn btn-primary">Save item</button>
+  </form>
   <ul>
-    <li v-for="item in items" :key="item.id">{{ item.label }}></li>
+    <li v-for="item in items" :key="item.id">{{ item.label }}</li>
   </ul>
 </template>
