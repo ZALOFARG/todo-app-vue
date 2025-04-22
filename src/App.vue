8175b4c1f2d88fcaa67626e8ab8e9a1043df0c79
ZALOFARG <andre.work.go@gmail.com>
2025-04-22 10:37:40 -0500

    feat: provide disable func to the button

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,17 @@ const saveItem = () => {
   items.value.push({ id: items.value.length + 1, label: newItem.value })
   newItem.value = ''
 }
+const doEdit = (e) => {
+  editing.value = e
+  newItem.value = ''
+}
 </script>
 
 <template>
   <div class="header">
     <h1>{{ header }}</h1>
-    <button v-if="editing" class="btn">Cancel</button>
-    <button v-else class="btn btn-primary">Add Item</button>
+    <button v-if="editing" class="btn" @click="doEdit(false)">Cancel</button>
+    <button v-else class="btn btn-primary" @click="doEdit(true)">Add Item</button>
   </div>
   <form class="add-item-form" @submit.prevent="saveItem" v-if="editing">
     <input v-model.trim="newItem" type="text" placeholder="Add an item..." />
@@ -28,7 +32,7 @@ const saveItem = () => {
       <input type="checkbox" v-model="newItemHighPriority" />
       Priority
     </label>
-    <button class="btn btn-primary">Save item</button>
+    <button class="btn btn-primary" :disabled="newItem.length === 0">Save item</button>
   </form>
   <ul>
     <li v-for="item in items" :key="item.id">{{ item.label }}</li>
